93b6ba437743e3c75918f70ebc9890c7f1cb6772
MattD21 <86857432+MattD21@users.noreply.github.com>
2023-06-02 20:45:04 -0500

    Delete ASESP23 directory

--- ASESP23/import-parts.php
+++
@@ -1,8 +0,0 @@
-<?php
-$files=array('equip-ai','equip-aj','equip-ak'); //USE WHATEVER FILES YOU CREATED IN THE FILE SPLIT
-foreach($files as $key=>$value)
-{
-    shell_exec("/usr/bin/php /var/www/html/import-args.php $key $value > /var/www/html/$value.log 2>/var/www/html/$value.log &");
-}
-echo "Main Process Done\n";
-?>
